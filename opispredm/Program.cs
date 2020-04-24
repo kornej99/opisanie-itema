9a8a7ebdde7d96d7a24a3110f122ed4a1de32d56
kornej99 <56335230+kornej99@users.noreply.github.com>
2020-04-24 22:10:40 +0400

    Add files via upload

---
+++ opispredm/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace opispredm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ opispredm/myach.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace description_of_item
+{
+    class myach
+    {
+        public int weight { get; set; }
+        public string type { get; set; }
+        public int size { get; set; }
+        public double speed { get; set; }
+        public string material { get; set; }
+
+        string mat1 = "Камень";
+        int HP = 50;
+        public void Uron()
+        {
+            if (speed > 10)
+            {
+                HP -= 5;
+            }
+            else
+            {
+                smert();
+            }
+        }
+        public string smert()
+        {
+            if (material == mat1)
+            {
+                return "wasted";
+            }
+            return "";
+        }
+    }
+}
